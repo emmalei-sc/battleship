2316630044a782a6a73428657e9e0ecea31bd747
Jordan McWilliams <jmcwilli@usc.edu>
2022-11-29 11:34:28 -0800

    Update Server.java

--- src/battleship/Server.java
+++ src/battleship/Server.java
@@ -12,15 +12,16 @@ import java.io.*;
 //The main class for the backend
 public class Server extends HttpServlet {
 // Should this be a servlet instead?
-	
 	Queue<UserThread> users;
+	boolean isMatchRunning = false;
 	public Server(int port) {
 		users = new LinkedList<>();
-		this.run();
 		Match m;
 		while (true) {
-			if (users.size() >= 2) {
+			//listen for new users connecting, verify them, continually add them to the queue as UserThreads
+			if (!isMatchRunning && users.size() >= 2) {
 				m = new Match(users.remove(), users.remove());
+				isMatchRunning = true;
 			}
 			//update database
 		}
@@ -30,13 +31,17 @@ public class Server extends HttpServlet {
 		Server s = new Server(3456);
 	}
 	
+	public void UpdateMatchStatus(boolean status) {
+		isMatchRunning = status;
+	}
+	
 	public synchronized void addUser(UserThread user) {
 		users.add(user);
 	}
 	
-	public void run() {
+	/* public void run() {
 		while (true) {
 			//listen for new users connecting, verify them, continually add them to the queue as UserThreads
 		}
-	}
+	} */
 }
