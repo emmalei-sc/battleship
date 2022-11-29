7bf32b4a4f998ba91f2ed65b4830571b60a61c8d
Emma Leihe <leihe@usc.edu>
2022-11-29 14:51:55 -0800

    userthread

--- src/battleship/UserThread.java
+++ src/battleship/UserThread.java
@@ -1,19 +1,33 @@
 package battleship;
 
 public class UserThread extends Thread{
-	String username;
-	String password;
-	int wins;
-	int losses;
+	
+	private String username;
+	private String password;
+	private int wins = 0;
+	private int losses = 0;
+	private int ties = 0;
+	private int shipsSunk = 0;
+	private int shipsLost = 0;
+	
 	public UserThread() {
 		
 	}
-	public UserThread(String un, String pw, int w, int l) {
-		username = un;
-		password = pw;
-		wins = w;
-		losses = l;
+	
+	public UserThread(String username, String pw) {
+		this.username = username;
+		this.password = password;
 	}
+	
+	public UserThread(String username, String pw, int wins, int losses, int shipsSunk, int shipsLost) {
+		this.username = username;
+		this.password = password;
+		this.wins = wins;
+		this.losses = losses;
+		this.shipsSunk = shipsSunk;
+		this.shipsLost = shipsLost;
+	}
+	
 	public void run()
 	{
 		
