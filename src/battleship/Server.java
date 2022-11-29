37ffb4414564376bfbf18d5b835b61240617ea0c
Jordan McWilliams <jmcwilli@usc.edu>
2022-11-29 14:24:02 -0800

    AddUser Function
    New AddUser function that gets the User's info from the database and makes a new thread for them

--- src/battleship/Server.java
+++ src/battleship/Server.java
@@ -2,6 +2,7 @@ package battleship;
 
 import java.util.LinkedList;
 import java.util.Queue;
+import java.sql.*;
 
 import javax.servlet.http.HttpServlet;
 
@@ -11,19 +12,30 @@ import java.io.*;
 
 //The main class for the backend
 public class Server extends HttpServlet {
-// Should this be a servlet instead?
+	ServerSocket server;
+	Socket socket;
 	Queue<UserThread> users;
 	boolean isMatchRunning = false;
 	public Server(int port) {
 		users = new LinkedList<>();
 		Match m;
-		while (true) {
-			//listen for new users connecting, verify them, continually add them to the queue as UserThreads
-			if (!isMatchRunning && users.size() >= 2) {
-				m = new Match(users.remove(), users.remove());
-				isMatchRunning = true;
+		try {
+			server = new ServerSocket(port);
+			while (true) {
+				//listen for new users connecting, verify them, continually add them to the queue as UserThreads
+				socket = server.accept();
+				InetAddress inetAddress = socket.getInetAddress();
+				System.out.println("Connection from IP: " + inetAddress.getHostAddress());
+				UserThread newUser = new UserThread(); //TODO
+				users.add(newUser);
+				if (!isMatchRunning && users.size() >= 2) {
+					m = new Match(users.remove(), users.remove());
+					isMatchRunning = true;
+				}
+				//update database
 			}
-			//update database
+		} catch (IOException e) {
+			
 		}
 	}
 	
@@ -33,6 +45,35 @@ public class Server extends HttpServlet {
 	
 	public void UpdateMatchStatus(boolean status) {
 		isMatchRunning = status;
+	}
+	
+	public synchronized void addUser(String username) {
+		//Please look this function over and ensure the strings match to the database
+		Connection conn;
+		PreparedStatement ps;
+		ResultSet rs;
+		String password = null;
+		int wins = 0;
+		int losses = 0;
+		try {
+			Class.forName("conn.mysql.jdbc.Driver");
+			conn = DriverManager.getConnection("jdbc:mysql://localhost/Users?user=root&password=root&useSSL=false");
+			ps = conn.prepareStatement("SELECT * FROM Users WHERE userName=?");
+			ps.setString(1, username);
+			rs = ps.executeQuery();
+			while (rs.next()) {
+				password = rs.getString("password");
+				wins = rs.getInt("wins");
+				losses = rs.getInt("losses");
+			}
+			UserThread newUser = new UserThread(username, password, wins, losses);
+			users.add(newUser);
+		} catch (SQLException sqle) {
+			System.out.println(sqle.getMessage());
+		} catch (ClassNotFoundException cnfe) {
+			System.out.println(cnfe.getMessage());
+		}
+		
 	}
 	
 	public synchronized void addUser(UserThread user) {
--- src/battleship/UserThread.java
+++ src/battleship/UserThread.java
@@ -1,10 +1,19 @@
 package battleship;
 
 public class UserThread extends Thread{
+	String username;
+	String password;
+	int wins;
+	int losses;
 	public UserThread() {
 		
 	}
-	
+	public UserThread(String un, String pw, int w, int l) {
+		username = un;
+		password = pw;
+		wins = w;
+		losses = l;
+	}
 	public void run()
 	{
 		
